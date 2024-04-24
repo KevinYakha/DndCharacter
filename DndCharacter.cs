978fef1141928feb778ce9ae7ca8990bf28855f7
KevinYakha <kevin@yakha.eu>
2024-04-24 14:00:02 +0200

    DnDCharacters can now be automagically generated

--- DndCharacter.cs
+++ DndCharacter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 public class DndCharacter
 {
@@ -9,19 +10,40 @@ public class DndCharacter
     public int Wisdom { get; }
     public int Charisma { get; }
     public int Hitpoints { get; }
+    private static Random rng = new Random(DateTime.Now.Millisecond);
 
+    DndCharacter()
+    {
+        Strength = Ability();
+        Dexterity = Ability();
+        Constitution = Ability();
+        Intelligence = Ability();
+        Wisdom = Ability();
+        Charisma = Ability();
+        Hitpoints = 10 + Modifier(Constitution);
+    }
     public static int Modifier(int score)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Convert.ToInt32(Math.Floor(((float)score - 10) / 2));
     }
 
     public static int Ability() 
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int[] diceRolls = {
+            rng.Next(1,7),
+            rng.Next(1,7),
+            rng.Next(1,7),
+            rng.Next(1,7)
+        };
+        int lowestRollIndex = 0;
+        for (int i = 0, lowestRoll = diceRolls.Min() ; i < diceRolls.Length; i++)
+            lowestRollIndex = diceRolls[i] == lowestRoll ? i : lowestRollIndex;
+        diceRolls[lowestRollIndex] = 0;
+        return diceRolls.Sum();
     }
 
     public static DndCharacter Generate()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new DndCharacter();
     }
 }
